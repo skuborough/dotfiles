8b2037ced766a782fee2426d4677b5dc232b558e
Skub Lab <skub@outlook.com>
2026-07-05 14:24:59 +0900

    feat: add install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl enable --now docker
+sudo usermod -aG docker "${USER}"
